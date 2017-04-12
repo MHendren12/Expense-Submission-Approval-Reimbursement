1c57698f601b1e7c2b232da04fe854516e6d1628
Marek <mstolar2@oakland.edu>
2017-04-12 17:27:13 +0000

    update about us  page

--- aboutus.php
+++ aboutus.php
@@ -7,13 +7,13 @@
                         <div class="col-md-1">
                         </div>
                         <div class="col-md-10">
-                            <h3>Summary</h3>
+                            <h3>Our Story</h3>
                             <hr>
-                            <p>Our team consists of six members. We are a mix of Computer Science and IT students.
-                            houifshddsbfh dsfdsfdsgbildfs hglfvblfbvlkifs dbvblfd svbblb sdfvbfduhvbsdv bfdlisv bfkds vbflkv bdskfvbdfkl
-                            hfsd fdhsis gf udsh gifldsbg bvfa uaohf kfhidfs b fdhsf kdsfosdf ndljgf sdnfdsf dsf kfdsnb
-                            fdb ksf kdfb kjsdfb kuvd v if ndshyr fids  sdnb mfgonbf ,sdgofhsk figbfgdggnd dfb nfbudgb mndf ufdb sdff ds
-                            </p> 
+                            <p>The making of R.A.S.E (Reimbursement and Submission of Expenses)</p>
+                            <p>R.A.S.E service was launched, part of Senior Capstone Project class in 2017. In this class, students from various fields such as Computer Science and Information technology brought their skill and creativity into developing great projects that many people may find helpful with their business or daily lives.</p> 
+                            <p>This project started out from simple idea. Expense Submission / Approval and Reimbursement that many businesses rely on excel based expense reporting and approval system. The responsibility would be creating a system for expense pre-approval, reporting, post approval and reimbursement system. A key role in the development of this site was devoted to team of 6 people their names are Michael Hendren, Jon Calvert, Matt Papais, Shane Kuskowski Andre Mitchell and Marek Stolarczyk. Their hard work brought this simple idea into full working website called R.A.S.E.</p>
+                            <p>R.A.S.E service was created for small businesses by allowing for self-management of revenues and expenses. Reporting expense has never been a straightforward process. Not any more, with this system you will be able to organize time, pay of your employees, and manage many different items as travel, food, or equipment expense. This site has many functionalities that makes expense reports process a lot easier than working with paper and pen. Some of the features that R.A.S.E website provides: Allow for registration of users with different credential as trackers and submitters of their business expenses, the form can be customized up to the needs of company types of expense, Users with specific role will be able to view and approve expense, any message about pending or new forms will be sent to manager of the company, and other good features that will help speed the process of receiving your reimbursement.</p>
+                            <p>Thank you for visiting our website and inquiry to use our tool to make your work a lot easier. R.A.S.E team.</P>
                             <hr><br>
                             <h3>Our Goal and Vision</h3><hr>
                             <p>
